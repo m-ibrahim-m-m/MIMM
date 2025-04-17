d214b63083ca57f1c7cdd3e775f4d0fb77f08541
Mohamed Ibrahim Mohamed <129611823+m-ibrahim-m-m@users.noreply.github.com>
2025-04-17 16:29:22 +0200

    Update App2.py

--- App2.py
+++ App2.py
@@ -363,27 +363,6 @@ def plot_status_trends(filtered_data):
     )
     st.plotly_chart(fig2, use_container_width=True)
 
- # Create temporal aggregation
-    trend_data3 = filtered_data.groupby(
-        ['Year', 'Month', 'Order Status', 'Plan Type']
-        ).size().reset_index(name='Count2')
-    fig3 = px.line(
-        trend_data3,
-        x='Month',
-        y='Count2',
-        color='Order Status',
-        facet_col='Year',
-        markers=True,
-       category_orders={
-            "Plan Type": month_order
-        }
-    )
-    fig2.update_layout(
-        hovermode="x unified",
-        xaxis_title="Plan Type",
-        yaxis_title="Number of Orders"
-    )
-    st.plotly_chart(fig3, use_container_width=True)
 
 def plot_order_type_analysis(filtered_data):
     """Visualize order type analysis"""
