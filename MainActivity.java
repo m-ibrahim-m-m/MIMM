c80adfe6b11d898eddd044e568bb15ea25651e95
Mohamed Ibrahim Mohamed <129611823+m-ibrahim-m-m@users.noreply.github.com>
2025-04-21 22:41:07 +0200

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.prayertrackerapp;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+
+        // Load your deployed Streamlit app
+        webView.loadUrl("https://kidsprayertracker.streamlit.app/");
+    }
+}
